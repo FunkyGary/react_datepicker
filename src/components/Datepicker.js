083cafa85d69114c7289bd1b148f001198de5c84
Gary Huang <gary80212@gmail.com>
2019-04-11 12:31:52 +0800

    add Datepicker component

---
+++ src/components/Datepicker.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './DatePicker.css';
+
+class DatePicker extends React.PureComponent {
+    render() {
+        return 'Hello from DatePicker';
+    }
+}
+
+export default DatePicker;
